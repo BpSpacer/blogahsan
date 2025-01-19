367fffa2526f0841e0e6451307a28f72a52ec07e
Muhammad Ahsan Kamal <ahsankamal9954@gmail.com>
2025-01-19 15:44:13 +0500

    First

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ThemeProvider } from "./components/dashboard/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,8 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+          <Toaster richColors closeButton />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
