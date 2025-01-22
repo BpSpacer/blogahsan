31294348bbb8d13ca743bbc0f39584354df0db3a
Muhammad Ahsan Kamal <ahsankamal9954@gmail.com>
2025-01-22 22:14:20 +0500

    SEO Change

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./components/dashboard/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
-import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,8 +14,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "AdsByAhsan",
-  description: "Created with ❤️ By Ahsan",
+  title: "Blog Ahsan",
+  description: "Created by Ahsan",
 };
 
 export default function RootLayout({
@@ -26,17 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Created with ❤️ By Ahsan" />
-        <meta name="author" content="Ahsan" />
-        <meta name="keywords" content="ads, marketing, advertising, digital marketing" />
-        <meta property="og:title" content="AdsByAhsan" />
-        <meta property="og:description" content="Created with ❤️ By Ahsan" />
-        <meta property="og:image" content="@/public/logo-dark.pnh" />
-        <meta property="og:url" content="https://adsbyahsan.com" />
-        <link rel="canonical" href="https://adsbyahsan.com" />
-      </head>
       <body className={`${geistSans.className} ${geistMono.className}`}>
         <ThemeProvider
           attribute="class"
@@ -45,7 +33,6 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <Analytics />
           <Toaster richColors closeButton />
         </ThemeProvider>
       </body>
