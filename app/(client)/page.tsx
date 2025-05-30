d5c5f43793cea5b08a71281b3d10cbd2a2aaa220
BpSpacer <ahsankamal9954@gmail.com>
2025-05-30 15:02:36 +0500

    analytics

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
-export default function Page() {
-  return <div>Ahsan</div>;
+export default function ClientPage() {
+  return <div>Page is under construction.</div>;
 }
