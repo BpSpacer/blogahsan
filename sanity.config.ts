506319b5c7bf050be1ea85180d5c32ad4d51d0a3
BpSpacer <ahsankamal9954@gmail.com>
2025-05-30 11:49:57 +0500

    sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,21 +6,21 @@
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
-import { structure } from './sanity/structure'
 import { schema } from './sanity/schemaTypes'
+import {deskTool} from 'sanity/desk'
+
 
 export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-    structure({structure}),
-    // Vision is for querying with GROQ from inside the Studio
+    deskTool(),
+    // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
   ],
